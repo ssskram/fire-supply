f390c580e89060cc337200fd6604a1fca472c690
ssskram <paul.marks@pittsburghpa.gov>
2018-08-13 10:45:48 -0400

    removed getUserGroup function from account controller

--- Controllers/Account.cs
+++ Controllers/Account.cs
@@ -68,50 +68,6 @@ namespace firesupply.Controllers {
             }
         }
 
-        public async Task GetUserGroup () {
-            var MSurl = "https://accounts.accesscontrol.windows.net/f5f47917-c904-4368-9120-d327cf175591/tokens/OAuth/2";
-            var clientid = Environment.GetEnvironmentVariable ("SPClientID");
-            var clientsecret = Environment.GetEnvironmentVariable ("SPClientSecret");
-            var refreshtoken = Environment.GetEnvironmentVariable ("refreshtoken");
-            var redirecturi = Environment.GetEnvironmentVariable ("redirecturi");
-            var SPresource = Environment.GetEnvironmentVariable ("spresourceid");
-            client.DefaultRequestHeaders.Clear ();
-            client.DefaultRequestHeaders.Add ("Accept", "application/x-www-form-urlencoded");
-            client.DefaultRequestHeaders.Add ("X-HTTP-Method", "POST");
-
-            var json =
-                String.Format ("grant_type=refresh_token&client_id={0}&client_secret={1}&refresh_token={2}&redirect_uri={3}&resource={4}",
-                    clientid, // 0
-                    clientsecret, // 1
-                    refreshtoken, // 2
-                    redirecturi, // 3
-                    SPresource); // 4
-
-            client.DefaultRequestHeaders.Add ("ContentLength", json.Length.ToString ());
-            try {
-                StringContent strContent = new StringContent (json);
-                strContent.Headers.ContentType = MediaTypeHeaderValue.Parse ("application/x-www-form-urlencoded");
-                HttpResponseMessage response = client.PostAsync (MSurl, strContent).Result;
-                response.EnsureSuccessStatusCode ();
-                var content = await response.Content.ReadAsStringAsync ();
-                dynamic results = JsonConvert.DeserializeObject<dynamic> (content);
-                string accesstoken = results.access_token.ToString ();
-                var sharepointUrl = "https://cityofpittsburgh.sharepoint.com/sites/Fire/_api/web/lists/GetByTitle('SupplyUsers')/items";
-                client.DefaultRequestHeaders.Clear ();
-                client.DefaultRequestHeaders.Authorization =
-                    new AuthenticationHeaderValue ("Bearer", accesstoken);
-                client.DefaultRequestHeaders.Add ("Accept", "application/json;odata=verbose");
-                try {
-                    string group = await client.GetStringAsync (sharepointUrl);
-                    TempData["usergroup"] = group;
-                } catch (Exception ex) {
-                    System.Diagnostics.Debug.WriteLine (ex.Message);
-                }
-            } catch (Exception ex) {
-                System.Diagnostics.Debug.WriteLine (ex.Message);
-            }
-        }
-
         [HttpPost]
         [AllowAnonymous]
         public async Task<IActionResult> Logout () {
