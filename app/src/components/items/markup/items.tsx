2308d143cd2aa4bec3122f09d759e50f93de1446
ssskram <paul.marks@pittsburghpa.gov>
2019-01-03 13:24:45 -0500

    put items in table instead of card

--- app/src/components/items/markup/items.tsx
+++ app/src/components/items/markup/items.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import * as types from '../../../store/types'
+import ReactTable from "react-table"
+import "react-table/react-table.css"
 
 type props = {
     items: Array<types.item>
@@ -8,24 +10,41 @@ type props = {
 export default class ItemCards extends React.Component<props, {}>{
 
     render() {
-        const itemCards = this.props.items.map((item, key) => {
-            return (
-                <div className="col-lg-4 col-md-6 col-sm-12" key={key}>
-                    <div className="panel">
-                        <div className="panel-body text-center">
-                            <h4>{item.name}</h4>
-                            <h5><b>{item.type}</b></h5>
-                            <button className='btn btn-success'><span className='glyphicon glyphicon-plus'></span></button>
-                        </div>
-                    </div>
-                </div>
-            )
-        })
+        const columns = [{
+            Header: 'Item',
+            accessor: 'name'
+        }, {
+            Header: 'Type',
+            accessor: 'type',
+        }, {
+            Header: '',
+            accessor: 'cartegraphID',
+            Cell: props => <button className='btn btn-success' title='Add to cart'><span className='glyphicon glyphicon-plus'></span></button>,
+            maxWidth: 60
+        }]
 
         return (
-            <div className='row'>
-                {itemCards}
+            <div className='col-md-12'>
+                <ReactTable
+                    data={this.props.items}
+                    columns={columns}
+                    loading={false}
+                    minRows={0}
+                    pageSize={100}
+                    showPagination={true}
+                    showPageSizeOptions={false}
+                    noDataText=''
+                    getTdProps={() => ({
+                        style: {
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            fontSize: '14px'
+                        }
+                    })}
+                />
             </div>
         )
     }
+
 }
