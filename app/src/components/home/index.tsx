a78da2ad14b1417881fbd214222dac499c4151c1
ssskram <paul.marks@pittsburghpa.gov>
2019-02-04 11:02:56 -0500

    notice on homepage, dev mode

--- app/src/components/home/index.tsx
+++ app/src/components/home/index.tsx
@@ -7,6 +7,9 @@ export default class Home extends React.Component<any, any> {
     render() {
         return (
             <div>
+                <div className='alert alert-danger'>
+                    <b>Notice:</b> This site is not live, and contains only a snapshot of existing orders.  Currently, any orders placed through this site will not be filled.  Currently, this site is only configured for demo purposes and testing.
+                </div>
                 <HydrateStore />
                 <Items />
             </div>
