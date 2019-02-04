1b13a779431fc4ca1d3ad9cbeb9787fa8839a0cf
ssskram <paul.marks@pittsburghpa.gov>
2019-02-04 12:20:42 -0500

    sike one more, verbage and stuff

--- app/src/components/home/index.tsx
+++ app/src/components/home/index.tsx
@@ -9,7 +9,7 @@ export default class Home extends React.Component<any, any> {
         return (
             <div>
                 <div className='alert alert-danger'>
-                    <b>Notice:</b> This site is not live, and contains only a snapshot of existing orders.  Currently, any orders placed through this site will not be filled.  Currently, this site is only configured for demo purposes and testing.  So...feel free to play around, submit orders, and try to break things <span><img style={{ height: '20px' }} src={smiley as string} /></span>
+                    <b>Notice:</b> This site is not live, and contains only a snapshot of existing orders.  Currently, any orders placed through this site will not be filled. This site is currently configured for demo and testing purposes only.  So...feel free to play around, submit orders, and try to break things <span><img style={{ height: '20px' }} src={smiley as string} /></span>
                 </div>
                 <HydrateStore />
                 <Items />
