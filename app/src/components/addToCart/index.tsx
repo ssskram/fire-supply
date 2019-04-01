d6ff374227aa69a22933a5a605e39b2d19c02b89
ssskram <ssskram@ssskram.fios-router.home>
2019-03-31 21:27:03 -0400

    LIVE

--- app/src/components/addToCart/index.tsx
+++ app/src/components/addToCart/index.tsx
@@ -1,74 +1,82 @@
-import * as React from 'react'
-import * as types from '../../store/types'
-import * as Style from './style'
-import Modal from 'react-responsive-modal'
-import SetQuantity from './itemQuantity'
-import ErrorHandler from '../../functions/errorHandler'
+import * as React from "react";
+import * as types from "../../store/types";
+import * as Style from "./style";
+import Modal from "react-responsive-modal";
+import SetQuantity from "./itemQuantity";
+import ErrorHandler from "../../functions/errorHandler";
 
 type props = {
-    user: types.user
-    userProfile: types.userProfile
-    item: types.item
-    updateCart: (obj) => void
-}
+  user: types.user;
+  userProfile: types.userProfile;
+  item: types.item;
+  updateCart: (obj) => void;
+};
 
 type state = {
-    setQuantity: boolean
-}
+  setQuantity: boolean;
+};
 
 export default class AddToCart extends React.Component<props, state> {
-    constructor(props) {
-        super(props)
-        this.state = {
-            setQuantity: false,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      setQuantity: false
+    };
+  }
+
+  newCart(cartItem) {
+    try {
+      let newCart = this.props.userProfile.cart;
+      newCart.push(cartItem);
+      const newUserProfile = {
+        user: this.props.user.email,
+        cart: newCart
+      };
+      this.props.updateCart(newUserProfile);
+      this.setState({
+        setQuantity: false
+      });
+    } catch (err) {
+      ErrorHandler(err);
     }
+  }
 
-    newCart(cartItem) {
-        try {
-            let newCart = this.props.userProfile.cart
-            newCart.push(cartItem)
-            const newUserProfile = {
-                user: this.props.user.email,
-                cart: newCart
-            }
-            this.props.updateCart(newUserProfile)
-            this.setState({
-                setQuantity: false
-            })
-        } catch (err) { ErrorHandler(err) }
-    }
+  render() {
+    const { setQuantity } = this.state;
 
-    render() {
-        const {
-            setQuantity
-        } = this.state
+    const { item } = this.props;
 
-        const {
-            item
-        } = this.props
-
-        return (
-            <div>
-                <button onClick={() => this.setState({ setQuantity: true })} className='btn btn-success' title='Add to cart'>
-                    <span className='glyphicon glyphicon-plus' style={{ marginRight: '-2px' }}></span>
-                </button>
-                {setQuantity &&
-                    <Modal
-                        open={true}
-                        onClose={() => { this.setState({ setQuantity: false }) }}
-                        classNames={Style.modalClasses}
-                        showCloseIcon={true}
-                        center>
-                        <SetQuantity
-                            quantity={0}
-                            item={item}
-                            type={'Add to cart'}
-                            returnQuantity={this.newCart.bind(this)}
-                        />
-                    </Modal>
-                }
-            </div>
-        )
-    }
+    return (
+      <div>
+        <button
+          onClick={() => this.setState({ setQuantity: true })}
+          className="btn btn-success"
+          title="Add to cart"
+        >
+          <span
+            className="glyphicon glyphicon-plus"
+            style={{ marginRight: "-2px" }}
+          />
+        </button>
+        {setQuantity && (
+          <Modal
+            open={true}
+            onClose={() => {
+              this.setState({ setQuantity: false });
+            }}
+            classNames={Style.modalClasses}
+            showCloseIcon={true}
+            center
+          >
+            <SetQuantity
+              quantity={0}
+              item={item}
+              type={"Add to cart"}
+              returnQuantity={this.newCart.bind(this)}
+            />
+          </Modal>
+        )}
+      </div>
+    );
+  }
 }
--- app/src/components/userProfile/markup/setProfile.tsx
+++ app/src/components/userProfile/markup/setProfile.tsx
@@ -6,7 +6,7 @@ export default class SetProfile extends React.Component {
     render() {
         return (
             <div className='text-center'>
-                <h2>Welcome to PGH Supply</h2>
+                <h2 className='ubuntu'>Welcome to PGH Supply</h2>
                 <h4>First, select your department or bureau</h4>
                 <Select />
             </div>
